141f9415b8e67b2e9d3d3b59338f2e72e9e55147
Maya Tunde Olawale <mayadihno@gmail.com>
2025-06-20 18:51:09 +0100

    Add ScrollToTop component and configure Netlify redirects

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import ScrollToTop from "./static/ScrollTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/static/icons.js
@@ -0,0 +1,41 @@
+import { FaInstagram, FaFacebookF } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa6";
+import { BsTwitterX } from "react-icons/bs";
+import { IoLogoYoutube } from "react-icons/io";
+import { CiSearch, CiMenuBurger } from "react-icons/ci";
+import {
+  MdClose,
+  MdEmail,
+  MdAccessTime,
+  MdAdd,
+  MdOutlineChevronLeft,
+  MdOutlineChevronRight,
+} from "react-icons/md";
+import { PiPhoneCallThin } from "react-icons/pi";
+import { BsSend } from "react-icons/bs";
+import { LuMinus } from "react-icons/lu";
+import { HiArrowLongRight } from "react-icons/hi2";
+import { IoIosCheckmark } from "react-icons/io";
+import { FaLinkedinIn } from "react-icons/fa6";
+
+export const ICONS = {
+  instagram: FaInstagram,
+  youtube: IoLogoYoutube,
+  search: CiSearch,
+  menu: CiMenuBurger,
+  close: MdClose,
+  email: MdEmail,
+  phone: PiPhoneCallThin,
+  x: BsTwitterX,
+  whatsapp: FaWhatsapp,
+  facebook: FaFacebookF,
+  send: BsSend,
+  time: MdAccessTime,
+  minus: LuMinus,
+  add: MdAdd,
+  arrow: HiArrowLongRight,
+  check: IoIosCheckmark,
+  linkedin: FaLinkedinIn,
+  left: MdOutlineChevronLeft,
+  right: MdOutlineChevronRight,
+};
